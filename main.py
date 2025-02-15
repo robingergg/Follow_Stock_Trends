4d9bc24189ef5df0145274183a3ec6e784fecf32
Robin Gerg <gergrobin@gmail.com>
2025-02-15 11:21:22 +0100

    FTS: call time series data collector according to the chosen time series

--- main.py
+++ main.py
@@ -28,17 +28,35 @@ async def get_stock_data(symbol: str, time_series: str, use_mock: bool = True) -
     This endpoint is used to get the stock data for a given symbol and time series.
     Example call: 'http://localhost:8000/stock/IBM?time_series=monthly&use_mock=true'
     """
+
+    time_series_callback = None
+    data = None
     try:
         # Collect data
         processor = StockDataProcessor()
 
         # TODO: create a look-up table for retrieving time_series value dynamically
         if time_series == "monthly":
-            data = processor.get_monthly_returns(symbol, use_mock=use_mock)
-            response = Response(content=json.dumps(data), media_type="application/json")
-            # NOTE: adding headers for CORS as it seems that add_middle doesnt properly allows all origin
-            response.headers["Access-Control-Allow-Origin"] = "*"
-            return response
+            time_series_callback = processor.get_monthly_returns(symbol, use_mock=use_mock)
+        elif time_series == "weekly":
+            time_series_callback = processor.get_weekly_returns(symbol, use_mock=use_mock)
+        elif time_series == "daily":
+            time_series_callback = processor.get_daily_returns(symbol, use_mock=use_mock)
+        else:
+            raise ValueError(f"Invalid time series: {time_series}")
+
+        if time_series_callback:
+            data = time_series_callback()
+        else:
+            raise ValueError(f"Invalid time series: {time_series}")
+
+        if not data:
+            raise ValueError("Unexpected error: No data returned")
+
+        response = Response(content=json.dumps(data), media_type="application/json")
+        # NOTE: adding headers for CORS as it seems that add_middle doesnt properly allows all origin
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        return response
 
     # TODO: create more detaled error(s)
     except Exception as e:
